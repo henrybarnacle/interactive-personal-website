e8cc0877407e81b065f3459b11c8e7e9c4a44def
Henry Barnacle <henrybarnacle@Henrys-MBP.fios-router.home>
2017-03-18 17:38:24 -0400

    social links added

---
+++ script.js
@@ -0,0 +1,156 @@
+$(window).on('beforeunload', function(){
+    $(window).scrollTop(0);
+  });
+
+ $('body').addClass('stop-scrolling');
+
+ $('.open-overlay').click(function() {
+
+  $('body').removeClass('stop-scrolling'); 
+
+       $('.open-overlay').css('pointer-events', 'none');
+       var overlay_navigation = $('.overlay-navigation'),
+         top_bar = $('.bar-top'),
+         middle_bar = $('.bar-middle'),
+         bottom_bar = $('.bar-bottom');
+
+       overlay_navigation.toggleClass('overlay-active');
+       if (overlay_navigation.hasClass('overlay-active')) {
+
+         top_bar.removeClass('animate-out-top-bar').addClass('animate-top-bar');
+         middle_bar.removeClass('animate-out-middle-bar').addClass('animate-middle-bar');
+         bottom_bar.removeClass('animate-out-bottom-bar').addClass('animate-bottom-bar');
+         overlay_navigation.removeClass('overlay-slide-up').addClass('overlay-slide-down')
+         overlay_navigation.velocity('transition.slideLeftIn', {
+           duration: 300,
+           delay: 0,
+           begin: function() {
+             $('nav ul li').velocity('transition.perspectiveLeftIn', {
+               stagger: 150,
+               delay: 0,
+               complete: function() {
+                 $('nav ul li a').velocity({
+                   opacity: [1, 0],
+                 }, {
+                   delay: 10,
+                   duration: 140
+                 });
+                 $('.open-overlay').css('pointer-events', 'auto');
+               }
+             })
+           }
+         })
+
+       } else {
+         $('.open-overlay').css('pointer-events', 'none');
+         top_bar.removeClass('animate-top-bar').addClass('animate-out-top-bar');
+         middle_bar.removeClass('animate-middle-bar').addClass('animate-out-middle-bar');
+         bottom_bar.removeClass('animate-bottom-bar').addClass('animate-out-bottom-bar');
+         overlay_navigation.removeClass('overlay-slide-down').addClass('overlay-slide-up')
+         $('nav ul li').velocity('transition.perspectiveRightOut', {
+           stagger: 150,
+           delay: 0,
+           complete: function() {
+             overlay_navigation.velocity('transition.fadeOut', {
+               delay: 0,
+               duration: 300,
+               complete: function() {
+                 $('nav ul li a').velocity({
+                   opacity: [0, 1],
+                 }, {
+                   delay: 0,
+                   duration: 50
+                 });
+                 $('.open-overlay').css('pointer-events', 'auto');
+               }
+             });
+           }
+         })
+      }
+    });
+ // Attach a click event to the "nav-link" class
+$(".nav-link").on("click", function() {
+  $('.open-overlay').css('pointer-events', 'none');
+       var overlay_navigation = $('.overlay-navigation'),
+         top_bar = $('.bar-top'),
+         middle_bar = $('.bar-middle'),
+         bottom_bar = $('.bar-bottom');
+
+       overlay_navigation.toggleClass('overlay-active');
+       if (overlay_navigation.hasClass('overlay-active')) {
+
+         top_bar.removeClass('animate-out-top-bar').addClass('animate-top-bar');
+         middle_bar.removeClass('animate-out-middle-bar').addClass('animate-middle-bar');
+         bottom_bar.removeClass('animate-out-bottom-bar').addClass('animate-bottom-bar');
+         overlay_navigation.removeClass('overlay-slide-up').addClass('overlay-slide-down')
+         overlay_navigation.velocity('transition.slideLeftIn', {
+           duration: 300,
+           delay: 0,
+           begin: function() {
+             $('nav ul li').velocity('transition.perspectiveLeftIn', {
+               stagger: 150,
+               delay: 0,
+               complete: function() {
+                 $('nav ul li a').velocity({
+                   opacity: [1, 0],
+                 }, {
+                   delay: 10,
+                   duration: 140
+                 });
+                 $('.open-overlay').css('pointer-events', 'auto');
+               }
+             })
+           }
+         })
+
+       } else {
+         $('.open-overlay').css('pointer-events', 'none');
+         top_bar.removeClass('animate-top-bar').addClass('animate-out-top-bar');
+         middle_bar.removeClass('animate-middle-bar').addClass('animate-out-middle-bar');
+         bottom_bar.removeClass('animate-bottom-bar').addClass('animate-out-bottom-bar');
+         overlay_navigation.removeClass('overlay-slide-down').addClass('overlay-slide-up')
+         $('nav ul li').velocity('transition.perspectiveRightOut', {
+           stagger: 150,
+           delay: 0,
+           complete: function() {
+             overlay_navigation.velocity('transition.fadeOut', {
+               delay: 0,
+               duration: 100,
+               complete: function() {
+                 $('nav ul li a').velocity({
+                   opacity: [0, 1],
+                 }, {
+                   delay: 0,
+                   duration: 50
+                 });
+                 $('.open-overlay').css('pointer-events', 'auto');
+               }
+             });
+           }
+         })
+      }
+    });
+  $(document).ready(function () {
+    // bind click event to all internal page anchors
+    $('a[href*="#"]').on('click', function (e) {
+        // prevent default action and bubbling
+        e.preventDefault();
+        e.stopPropagation();
+        // set target to anchor's "href" attribute
+        var target = $(this).attr('href');
+        // scroll to each target
+        $(target).velocity('scroll', {
+            delay: 1000,
+            duration: 1000,
+            easing: 'ease-in-out'
+        });
+    });
+
+});
+
+
+$( document ).on('click', function() {
+  
+  $('.list-2 li').velocity("transition.slideLeftIn", {delay: 1000, stagger: 75, drag: true });
+  
+});
